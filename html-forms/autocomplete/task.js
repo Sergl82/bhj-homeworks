e9f200c2564aa2c6e7206d5ace6e69997fd8c935
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-11-13 23:22:15 +1100

    html-forms

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,15 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    const similarWords = [];
+    const wordList = Array.from(this.input.options);
+    wordList.forEach((item) => {
+      if (item.text.includes(text)) {
+        similarWords.push({ 'text': item.text, 'value': item.value });
+      }
+    })
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return similarWords;
   }
 }
 
