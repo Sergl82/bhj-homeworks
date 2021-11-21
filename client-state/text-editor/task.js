ae5874eeecfa347c83c614afce613a79aa3208d3
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-11-22 07:36:36 +1100

    client-state

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,12 @@
+const textArea = document.querySelector('#editor');
+textArea.value = localStorage.getItem('area');
+const clearButton = document.querySelector("button");
+
+textArea.addEventListener('input', () => {
+    localStorage.setItem('area', textArea.value);
+});
+
+clearButton.addEventListener('click', () => {
+    textArea.value = "";
+    localStorage.removeItem('area');
+});
