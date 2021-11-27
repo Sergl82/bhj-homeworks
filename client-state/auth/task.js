ee074a411d7314f8f21f698c4f97446f8d7d1b5a
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-11-27 17:52:46 +1100

    client-state

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,49 +1,48 @@
 const formContainer = document.querySelector('.signin');
 const welcome = document.querySelector('.welcome');
+const form = document.querySelector('#signin__form');
+const logout = welcome.querySelector('.logout__btn');
 
 window.addEventListener('DOMContentLoaded', () => {
     if (localStorage.user === 'active') {
-        let welcome = document.querySelector('.welcome');
         welcome.classList.add('welcome_active');
         welcome.querySelector('#user_id').textContent = localStorage.userId;
-    } else {
-        formContainer.classList.add('signin_active');
-        const form = document.querySelector('#signin__form');
-        const btn = document.querySelector('#signin__btn');
+        formContainer.classList.remove('signin_active');
+    } else if(localStorage.user !== 'active'){
+      localStorage.clear();
+        }
+})
 
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            const login = form.querySelector('.control[name="login"]');
-            const password = form.querySelector('.control[name="password"]');
-            if (login.value === '' || password.value === '') {
-                return;
-            }
-            let formData = new FormData(form);
-            let request = new XMLHttpRequest();
-            request.open('POST', "https://netology-slow-rest.herokuapp.com/auth.php");
-            request.send(formData);
-            request.addEventListener('load', (event) => {
-                if (event.target.status === 200 && event.target.readyState === 4) {
-                    let response = JSON.parse(event.target.response);
-                    if (response.success === true) {
-                        formContainer.classList.remove('signin_active');
-                        let userId = response.user_id;
-                        welcome.classList.add('welcome_active');
-                        welcome.querySelector('#user_id').textContent = userId;
-                        localStorage.setItem('user', 'active');
-                        localStorage.setItem('userId', userId);
-                    } else {
-                        alert('Не правильный логин или пароль');
-                    }
-                } else {
-                    alert('Запрос не прошел');
-                }
-            });
-        });
-    }
-});
+form.addEventListener('submit', (event) => {
+         event.preventDefault();
+         const login = form.querySelector('.control[name="login"]');
+         const password = form.querySelector('.control[name="password"]');
+         if (login.value === '' || password.value === '') {
+           return;
+         }
+         let formData = new FormData(form);
+         let request = new XMLHttpRequest();
+         request.open('POST', "https://netology-slow-rest.herokuapp.com/auth.php");
+         request.send(formData);
+         request.addEventListener('load', (event) => {
+           if (event.target.status === 200 && event.target.readyState === 4) {
+             let response = JSON.parse(event.target.response);
+               if (response.success === true) {
+                 formContainer.classList.remove('signin_active');
+                 let userId = response.user_id;
+                 welcome.classList.add('welcome_active');
+                 welcome.querySelector('#user_id').textContent = userId;
+                 localStorage.setItem('user', 'active');
+                 localStorage.setItem('userId', userId);
+               } else {
+                 alert('Не правильный логин или пароль');
+               }
+             } else {
+                 alert('Запрос не прошел');
+             }
+         });
+      });
 
-let logout = welcome.querySelector('.logout__btn');
 logout.addEventListener('click', (event) => {
     event.preventDefault();
     localStorage.removeItem('user');
